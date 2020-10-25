d94d33260a531f2d8f81c0063c7e0735ad529e4b
i201820189 <Giomarlimocaldas1234@gmail.com>
2020-10-25 18:25:02 -0500

    Login Version 0.1

---
+++ Proyecto/Proyecto/Proyecto/Controllers/UsuarioController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using Proyecto.Models;
+
+namespace Proyecto.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        //Cambian de Conexion
+        SqlConnection con = new SqlConnection("server=GONDAR\\MSSQLSERVER01;database=COMPULAST;Integrated Security=true");
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult iniciarSesion(string codigo, string pass)
+        {
+            int resultado = -1;
+
+            SqlCommand sqlCommand = new SqlCommand("SP_BUSCAR_USUARIO", con);
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+            sqlCommand.Parameters.AddWithValue("@p_usuari_codigo", codigo);
+            sqlCommand.Parameters.AddWithValue("@p_usuari_pass", pass);
+
+            con.Open();
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+            if (sqlDataReader.Read())
+            {
+                Usuario usuario = new Usuario();
+                usuario.codigo = sqlDataReader.GetString(0);
+                usuario.pass = sqlDataReader.GetString(1);
+                usuario.apellidoPaterno = sqlDataReader.GetString(2);
+                usuario.apellidoMaterno = sqlDataReader.GetString(3);
+                usuario.nombres = sqlDataReader.GetString(4);
+                usuario.correo = sqlDataReader.GetString(5);
+                usuario.telefono = sqlDataReader.GetString(6);
+                resultado = 1;
+            }
+            sqlDataReader.Close();
+            con.Close();
+
+            if (resultado == 1)
+            {
+                return RedirectToAction("Index", "Home", new { USUARIO = codigo });
+            }
+            else
+            {
+                TempData["Error"] = "Usuario y/o contraseña incorrecta";
+                return RedirectToAction("Index");
+            }
+        }
+
+
+    }
+}
---
+++ Proyecto/Proyecto/Proyecto/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto.Models
+{
+    public class Usuario
+    {
+        public string codigo { get; set; }
+        public string pass { get; set; }
+        public string apellidoPaterno { get; set; }
+        public string apellidoMaterno { get; set; }
+        public string nombres { get; set; }
+        public string correo { get; set; }
+        public string telefono { get; set; }
+    }
+
+}
